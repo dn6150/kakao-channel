23d82fc197bfdb150449a938238e09407faff814
dn6150 <dn6150@naver.com>
2021-08-16 19:13:38 +0900

    메시지내용

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/api/sayHello', function(req, res, next){
+const responseBody = {
+version: "2.0",
+template: {
+outputs: [
+{
+simpleText: {
+text: "안녕"
+}
+}
+]
+}
+};
+console.log(req.body)
+res.status(200).send(responseBody);
+});
+module.exports = router;
