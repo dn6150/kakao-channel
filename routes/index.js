04b95a372dfbdfff2f76f93deb7c4c5ffcae916a
dn6150 <danool0821@gmail.com>
2021-08-17 17:19:16 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/api/sayHello', function(req, res, next){
-const responseBody = {
-version: "2.0",
-template: {
-outputs: [
-{
-simpleText: {
-text: "안녕"
-}
-}
-]
-}
-};
-console.log(req.body)
-res.status(200).send(responseBody);
-});
+	const responseBody = {
+	version: "2.0",
+	template: {
+	outputs: [
+		{
+		simpleText: {
+		text:"안녕"
+		}
+		}
+		]
+		}
+		};
+	console.log(req.body)
+	res.status(200).send(responseBody);
+	});
 module.exports = router;
